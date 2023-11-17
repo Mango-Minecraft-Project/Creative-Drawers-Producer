475706d53cd949879ef47608e5bc3ec6dd7e0fdf
Mango Jelly Pudding <evan940415@gmail.com>
2023-11-17 10:59:11 +0800

    rename, update packer.py

---
+++ modpacker.py
@@ -0,0 +1,25 @@
+import zipfile
+import os
+
+zipped_dirs = [
+    "overrides",
+    "manifest.json",
+    "modlist.html",
+    "README.md",
+]
+MODPACK_NAME = "Creative-Drawers-Producer"
+VERSION = "m1.0.5"
+
+print(f"> Start Packing Modpack - {MODPACK_NAME} {VERSION}")
+with zipfile.ZipFile(f"{MODPACK_NAME}-{VERSION}.zip", "w") as archive:
+    for directory in zipped_dirs:
+        if os.path.isdir(directory):
+            for root, _, files in os.walk(directory):
+                for file in files:
+                    _path = os.path.join(root, file)
+                    archive.write(_path)
+                    print(f'>...> Packing "{_path}"')
+        else:
+            archive.write(directory)
+            print(f'>...> Packing "{directory}"')
+print("> Modpack Packing Finished")
